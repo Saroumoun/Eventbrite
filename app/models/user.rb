ff79ed01e885db37f71cf9c10b675c5e668f10b4
Saroumoun <bastien.hiel@gmail.com>
2019-02-18 16:23:28 +0100

    added sendgrid

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
+	after_create :welcome_send
+
 	has_many :event_creations, foreign_key: 'admin_id', class_name: "Event"
 	has_many :event_attendances, foreign_key: 'attendee_id', class_name: "Attendance"
 	has_many :events, through: :event_attendances
+
+	def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
