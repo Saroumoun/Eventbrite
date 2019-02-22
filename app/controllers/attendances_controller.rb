75cd6ea7115f6f6c2cbbb2c993172bb7b45b839d
Saroumoun <bastien.hiel@gmail.com>
2019-02-22 13:51:07 +0100

    stripe

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,5 +1,37 @@
 class AttendancesController < ApplicationController
+	def index
+		@event = Event.find(params[:event_id])
+		@attendances = @event.attendees
+	end 
+
 	def new
+  	@event = Event.find(params[:event_id])
   	@attendance = Attendance.new
   end
+
+  def create
+  	@event = Event.find(params[:event_id])
+
+	  @amount = @event.price * 100
+
+	  customer = Stripe::Customer.create(
+	    :email => params[:stripeEmail],
+	    :source  => params[:stripeToken]
+	  )
+
+	  charge = Stripe::Charge.create(
+	    :customer    => customer.id,
+	    :amount      => @amount,
+	    :description => 'Rails Stripe customer',
+	    :currency    => 'eur'
+	  )
+
+	  @attendance = Attendance.create(stripe_customer_id: charge[:customer], attendee: current_user, event: @event) unless charge[:customer].nil?
+	  flash[:notice] = "You are now registred for this event"
+	  redirect_to @event
+
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to new_event_attendance_path(@event.id)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
 
   resources :events do 
   	resources :attendances
-  	resources :charges, only: [:new, :create]
   end
   
   resources :users, only: [:show]
