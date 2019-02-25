accc8aa89aa0138d9d6506def48a30fbead89c00
Saroumoun <bastien.hiel@gmail.com>
2019-02-25 21:42:55 +0100

    added index for admin to handle users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,33 @@
 class Admin::UsersController < ApplicationController
+	before_action :authenticate_user!
+  before_action :check_if_admin
+
+	def index
+    @users = User.all
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+  end
+  
+
+  private
+
+  	def check_if_admin
+  		unless current_user.is_admin
+	      flash[:danger] = "You are not admin"
+	      redirect_back(fallback_location: root_path)
+    	end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
 
 	namespace :admin do
 		root 'admin#index'
+		resources :users, only: [:index, :edit, :update, :destroy]
 	end
 end
