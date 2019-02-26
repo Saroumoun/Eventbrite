594c29cc7031789b1a52c559619889148ff36072
Saroumoun <bastien.hiel@gmail.com>
2019-02-26 12:22:34 +0100

    added dependent destroy on event to fix a bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
          
 	after_create :welcome_send
 
-	has_many :event_creations, foreign_key: 'admin_id', class_name: "Event"
-	has_many :event_attendances, foreign_key: 'attendee_id', class_name: "Attendance"
-	has_many :events, through: :event_attendances
+	has_many :event_creations, foreign_key: 'admin_id', class_name: "Event", dependent: :destroy
+	has_many :event_attendances, foreign_key: 'attendee_id', class_name: "Attendance", dependent: :destroy
+	has_many :events, through: :event_attendances, dependent: :destroy
   has_one_attached :profil_picture
 
 	def welcome_send
