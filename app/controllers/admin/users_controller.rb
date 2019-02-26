583249ff92137f470700993aac150ad7f8029b01
Saroumoun <bastien.hiel@gmail.com>
2019-02-26 11:39:44 +0100

    admin can delete user account

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,7 +3,7 @@ class Admin::UsersController < ApplicationController
   before_action :check_if_admin
 
 	def index
-    @users = User.all
+    @users = User.all.sort
   end
 
   def edit
@@ -16,9 +16,27 @@ class Admin::UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
+    if @user.update(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      description: params[:description]
+      )
+      if params[:is_admin].present?
+        @user.update(is_admin: params[:is_admin])
+      end
+
+      flash[:notice] = "User's info were updated"
+      redirect_to admin_users_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:notice] = "User succesfully deleted"
+    redirect_to admin_users_path
   end
   
 
